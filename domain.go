b644fc98c36435fc8acdb2c19178a1dd89039c93
Pryz <ju.pryz@gmail.com>
2016-09-15 12:48:34 -0700

    pinvpcu

--- domain.go
+++ domain.go
@@ -454,7 +454,8 @@ func (d *VirDomain) SetVcpusFlags(vcpu uint, flags uint) error {
 }
 
 func (d *VirDomain) PinVcpu(vcpu uint, cpumap []byte, maplen int) error {
-	result := C.virDomainPinVcpu(d.ptr, C.uint(vcpu), (*C.uchar)(unsafe.Pointer(&cpumap)), C.int(maplen))
+	//result := C.virDomainPinVcpu(d.ptr, C.uint(vcpu), (*C.uchar)(unsafe.Pointer(&cpumap)), C.int(maplen))
+	result := C.virDomainPinVcpu(d.ptr, C.uint(vcpu), (*C.uchar)(cpumap), C.int(maplen))
 	if result == -1 {
 		return GetLastError()
 	}
